f1eb803bcd6a84c05217266e473c630154ca81fd
Micadri <micheroux.adrien.atc@gmail.com>
2026-07-16 16:04:22 +0200

    add env.js

---
+++ env.js
@@ -0,0 +1,7 @@
+/**
+ * Configuration des variables d'environnement de l'application.
+ * Ce fichier est exclu du dépôt distant via le fichier .gitignore.
+ */
+export const ENV = {
+    apiUrl: 'https://ingrwf13.cepegra-frontend.xyz/wp_4/wp-json' 
+};
